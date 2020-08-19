a7f4ae234718b7794ae07e2f5b6e5da5f74862b1
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 16:08:12 +0200

    added running and stamina

--- Scripts/PlayerMovement.cs
+++ Scripts/PlayerMovement.cs
@@ -8,6 +8,8 @@ public class PlayerMovement : MonoBehaviour {
     public Rigidbody2D rigidbody;
     Vector2 movement;
     public Animator animator;
+    public static float max_stamina = 100f;
+    public static float stamina = 100f;
 
     // Update is called once per frame
     void Update() {
@@ -24,6 +26,20 @@ public class PlayerMovement : MonoBehaviour {
     // gets called 50 times a second, better for physics
     // normal Update gets called every frame, bad if frame rate changes
     void FixedUpdate() {
+        if (Input.GetButton("Jump") && stamina > 0) {
+            stamina -= 0.5f;
+            // small buffer so you cant hold space all the time and
+            // constantly switch between boosted and normal movement speed
+            if (stamina >= 10) {
+                this.movement_speed = 10f;
+            } 
+        }
+        else {
+            this.movement_speed = 5f;
+            if (stamina < 100f) {
+                stamina += 0.5f;
+            }
+        }
         // moving diagonally returns a magnitude of square root of 2, which would make diagonal movement 40% faster,
         // normalize movement to keep the same speed even when moving diagonally
         this.rigidbody.MovePosition(this.rigidbody.position + this.movement.normalized * this.movement_speed * Time.fixedDeltaTime);
