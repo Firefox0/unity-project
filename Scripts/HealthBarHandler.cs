6104c819b9b1b11bc6dc89015e58fcd6ff731ee8
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 15:27:47 +0200

    added healthbarhandler

---
+++ Scripts/HealthBarHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour {
+    public Slider slider;
+    public int health;
+    void Start() {
+        this.health = PlayerMain.health;
+        slider.maxValue = this.health;
+        slider.value = this.health;
+    }
+
+    void Update() {
+        if (PlayerMain.health != this.health) {
+            this.health = PlayerMain.health;
+            slider.value = this.health;
+        }    
+    }
+}
