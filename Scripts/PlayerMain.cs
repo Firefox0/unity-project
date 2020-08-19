1df2f6ac8a8c056d7227683521a39f6a99f5dfd8
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 12:30:47 +0200

    basic main script for collision and health

---
+++ Scripts/PlayerMain.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour {
+
+    public int health = 1;
+    public int gold = 0;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if(collider.tag == "Projectile") {
+            Destroy(collider.gameObject);
+            this.health -= 0;
+        }
+    }
+}
