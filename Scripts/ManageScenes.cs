3c6cab56a5251989eb2ac643b2b7230ed8fa104e
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 13:09:55 +0200

    basic scene manager to restart level

---
+++ Scripts/ManageScenes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManageScenes : MonoBehaviour {
+    void Update() {
+        if(PlayerMain.health <= 0) {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            // set manually to 1 so loading scene doesn't get called infinitely
+            PlayerMain.health = 1;
+        }
+    }
+}
