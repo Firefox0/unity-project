55c9ea9c6044d87131dc5693dff6188a9f041a04
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 13:42:02 +0200

    added attacker rotation

--- Scripts/AttackController.cs
+++ Scripts/AttackController.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class AttackController : MonoBehaviour {
 
     public GameObject projectile_prefab;
+    public GameObject player;
     System.Random random;
     GameObject[] attackers;
     public float projectile_force = 10f;
@@ -15,6 +16,13 @@ public class AttackController : MonoBehaviour {
         this.random = new System.Random();
         this.attackers = GameObject.FindGameObjectsWithTag("Attacker");
         StartCoroutine(this.initialize_attacks());
+    }
+
+    void FixedUpdate() {
+        foreach(GameObject attacker in this.attackers) {
+            attacker.transform.up = new Vector2(player.transform.position.x - attacker.transform.position.x,
+                                                player.transform.position.y - attacker.transform.position.y);
+        }
     }
 
     int[] random_unique_ints(int amount, int min, int max) {
@@ -37,20 +45,19 @@ public class AttackController : MonoBehaviour {
 
     IEnumerator initialize_attacks() {
         while (true) {
-            yield return new WaitForSeconds(5);
-            GameObject[] chosen_attackers = new GameObject[3];
+            yield return new WaitForSeconds(2);
             // pick random attackers and shoot projectile
             int[] random_ints = this.random_unique_ints(3, 0, this.attackers.Length - 1);
             foreach (int random_int in random_ints) {
                 GameObject chosen_attacker = this.attackers[random_int];
-                this.shoot_projectile(chosen_attacker.transform, chosen_attacker.transform.rotation);
+                this.shoot_projectile(chosen_attacker.transform);
             }
         }
     }
 
-    void shoot_projectile(Transform transform, Quaternion rotation) {
+    void shoot_projectile(Transform transform) {
         // create projectile and apply force to the rigidbody to shoot it
-        GameObject projectile = Instantiate(projectile_prefab, transform.position, rotation);
+        GameObject projectile = Instantiate(projectile_prefab, transform.position, transform.rotation);
         Rigidbody2D rigidbody = projectile.GetComponent<Rigidbody2D>();
         rigidbody.AddForce(transform.up * this.projectile_force, ForceMode2D.Impulse);
     }
