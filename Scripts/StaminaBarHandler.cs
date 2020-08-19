d4e871c2851a73648af7c051077b00650de25dd7
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 16:08:27 +0200

    staminabarhandler for HUD

---
+++ Scripts/StaminaBarHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBarHandler : MonoBehaviour {
+    public Slider slider;
+    public float stamina;
+    void Start() {
+        this.stamina = PlayerMovement.stamina;
+        this.slider.maxValue = this.stamina;
+        this.slider.value = this.stamina;
+    }
+
+    void Update() {
+        if (PlayerMovement.stamina != this.stamina) {
+            this.stamina = PlayerMovement.stamina;
+            this.slider.value = this.stamina;
+        }
+    }
+}
