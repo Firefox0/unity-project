775b3920b14bfa0000d8a36793aae5ebb0f3c656
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-19 16:07:56 +0200

    reset values properly

--- Scripts/ManageScenes.cs
+++ Scripts/ManageScenes.cs
@@ -7,8 +7,8 @@ public class ManageScenes : MonoBehaviour {
     void Update() {
         if(PlayerMain.health <= 0) {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-            // set manually to 1 so loading scene doesn't get called infinitely
             PlayerMain.health = PlayerMain.max_health;
+            PlayerMovement.stamina = PlayerMovement.max_stamina;
         }
     }
 }
