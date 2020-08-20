e0acd9b6544e18d0ba4fce67c590dbcccb5c2a68
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-20 12:18:09 +0200

    added comment

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMain : MonoBehaviour {
 
+    // Class which doesn't get destroyed despite scene reload/changes,
+    // important so we can remember things like gold or current level
+
     public GameObject player_main;
 
     public static int scene_index = 0;
