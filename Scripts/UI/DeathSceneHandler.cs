cf61e047db7b566da53f0cb86afb9dfbddf8753b
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-20 09:31:31 +0200

    folder for UI scripts

---
+++ Scripts/UI/DeathSceneHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathSceneHandler : MonoBehaviour {
+
+    public Button retry_button;
+    public Button shop_button;
+    public Button back_button;
+
+    void Start() {
+        this.retry_button.onClick.AddListener(() => SceneManager.LoadScene(PlayerMain.scene_index));
+        this.shop_button.onClick.AddListener(() => SceneManager.LoadScene(2));
+        this.back_button.onClick.AddListener(() => SceneManager.LoadScene(1));
+    }
+
+
+}
---
+++ Scripts/UI/LevelSelectorHandler.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelectorHandler : MonoBehaviour {
+
+    public Button level_1;
+    public Button back_button;
+
+    void Start() {
+        level_1.onClick.AddListener(() => {
+            PlayerMain.scene_index = 4;
+            SceneManager.LoadScene(4);
+        });
+        back_button.onClick.AddListener(() => SceneManager.LoadScene(0));
+    }
+}
