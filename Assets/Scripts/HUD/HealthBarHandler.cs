33dc99b27cd58fec1a9895aabc3a7cc937a4aeb9
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-20 11:37:02 +0200

    added entire project folder

---
+++ Assets/Scripts/HUD/HealthBarHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour {
+    public Slider slider;
+    public int health;
+    void Start() {
+        this.health = PlayerHit.health;
+        this.slider.maxValue = this.health;
+        this.slider.value = this.health;
+    }
+
+    void Update() {
+        if (PlayerHit.health != this.health) {
+            this.health = PlayerHit.health;
+            this.slider.value = this.health;
+        }    
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMain : MonoBehaviour {
+
+    public GameObject player_main;
+
+    public static int scene_index = 0;
+    public static int gold = 0;
+
+    void Start() {
+        DontDestroyOnLoad(player_main);
+    }
+}
