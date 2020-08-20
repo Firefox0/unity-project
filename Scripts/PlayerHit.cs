463a7ebf423f589b52d459991a7ccd8afc6c79e0
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-20 09:32:38 +0200

    created new script for hit detection and scene handling

---
+++ Scripts/PlayerHit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerHit : MonoBehaviour {
+
+    public GameObject player;
+
+    public static int max_health = 100;
+    public static int health = 100;
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if (collider.tag == "Projectile") {
+            Destroy(collider.gameObject);
+            health -= 20;
+            if (health <= 0) {
+                SceneManager.LoadScene(3);
+                health = max_health;
+                PlayerMovement.stamina = PlayerMovement.max_stamina;
+            }
+        }
+    }
+}
