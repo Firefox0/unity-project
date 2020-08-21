5832099a8d7c7708ca5a29529d73652be1857965
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-21 08:59:50 +0200

    renamed variable to remove warning

--- Assets/Scripts/Player/PlayerMain.cs
+++ Assets/Scripts/Player/PlayerMain.cs
@@ -3,16 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMain : MonoBehaviour {
-
-    // Class which doesn't get destroyed despite scene reload/changes,
-    // important so we can remember things like gold or current level
-
-    public GameObject player_main;
-
     public static int scene_index = 0;
     public static int gold = 0;
-
-    void Start() {
-        DontDestroyOnLoad(player_main);
-    }
 }
