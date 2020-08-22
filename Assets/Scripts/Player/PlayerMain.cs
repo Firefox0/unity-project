5a7a1318aca982c1fb2470d918676b77f79d2035
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-22 09:09:46 +0200

    renamed

--- Assets/Scripts/Player/PlayerMain.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerMain : MonoBehaviour {
-    public static int scene_index = 0;
-    public static int gold = 0;
-}
