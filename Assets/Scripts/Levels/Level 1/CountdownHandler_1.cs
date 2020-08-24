adb9806e22d3680a2bb82589148b2a46fb0113b8
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-24 22:01:57 +0200

    added level scripts

---
+++ Assets/Scripts/Levels/Level 1/CountdownHandler_1.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountdownHandler_1 : CountdownHandler {
+    void Awake() {
+        base.set_level(ref PlayerData.secrets.level_1.stars_earned);
+    }
+}
---
+++ Assets/Scripts/Levels/Level 2/PlayerHit_2.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class PlayerHit_2 : PlayerHit {
+    private void OnTriggerEnter2D(Collider2D collider) {
+        base.on_trigger(collider, ref PlayerData.secrets.level_2.stars_earned);
+    }
+}
