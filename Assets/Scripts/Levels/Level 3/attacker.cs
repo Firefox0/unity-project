710ec7423e913623a46f93ba790f532d9cbecbd4
Shao <shao-chieh.lee@campus.tu-berlin.de>
2020-08-25 21:14:47 +0200

    added Level-3

---
+++ Assets/Scripts/Levels/Level 3/attacker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class attacker : MonoBehaviour {
+
+    public GameObject crossbow;
+    public GameObject player;
+    public GameObject arrowPrefab;
+
+    private float fireRate = 2.0F;
+    private float nextArrow = 2.0F;
+
+    // Start is called before the first frame update
+    void Start() {
+        /*this.crossbow = new GameObject();*/
+    }
+
+    // Update is called once per frame
+    void Update() {
+        aimAtPlayer();
+        shoot();
+    }
+
+    public void aimAtPlayer() {
+        float playerX = player.transform.position.x;
+        float playerY = player.transform.position.y;
+
+        this.transform.up = new Vector2(playerX - this.transform.position.x, playerY - this.transform.position.y);
+    }
+
+    public void shoot() {
+        if (Time.time > nextArrow) {
+            nextArrow = Time.time + fireRate;
+            GameObject arrow = Instantiate(arrowPrefab, crossbow.transform.position, crossbow.transform.rotation);
+            Rigidbody2D rigidbody = arrow.GetComponent<Rigidbody2D>();
+            rigidbody.AddForce(transform.up * arrowPrefab.GetComponent<Basic_Projectile>().speed, ForceMode2D.Impulse);
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile/Arrow_Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Arrow_Projectile : Basic_Projectile {
+    
+    // Start is called before the first frame update
+    void Start() {
+    
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+
+    /*void travel(float velocity, Vector2 direction) {
+        this.GetComponent<Rigidbody2D>().velocity = direction * velocity;
+    }
+    void flyToDirection(Vector2 direction) {
+        this.GetComponent<Rigidbody2D>().velocity = direction;
+        //this.transform.Rotate(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+    }*/
+
+}
