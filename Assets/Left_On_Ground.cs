f7f90cd5810d1c54cdcbfde52d3a507711f08e85
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-25 10:17:12 +0200

    left on ground script to destroy object after specific time

---
+++ Assets/Left_On_Ground.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Left_On_Ground : MonoBehaviour {
+    public float time;
+    void Update() {
+        time -= Time.deltaTime;
+        if (time <= 0) {
+            Destroy(this.gameObject);
+        }
+    }
+}
