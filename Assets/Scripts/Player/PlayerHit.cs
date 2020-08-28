deab276ae00a7c1ae83df51440fe6daf9757c1f7
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-08-28 15:03:54 +0200

    fixed endless record bug

--- Assets/Scripts/Player/PlayerHit.cs
+++ Assets/Scripts/Player/PlayerHit.cs
@@ -21,7 +21,9 @@ public class PlayerHit : MonoBehaviour {
             health -= collider.gameObject.GetComponent<Basic_Projectile>().damage * amplifier;
             if (health <= 0) {
                 if (endless) {
-                    PlayerData.secrets.levels[current_level].record = Timer.current_time;
+                    if (Timer.current_time > PlayerData.secrets.levels[current_level].record) {
+                        PlayerData.secrets.levels[current_level].record = Timer.current_time;
+                    }
                 }
                 else {
                     // reference for readability
