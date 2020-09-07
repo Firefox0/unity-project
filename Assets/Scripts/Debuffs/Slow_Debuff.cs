808ee685ddc949f22e5dce63541e9c0858afa9ae
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-09-07 17:45:53 +0200

    improvements

--- Assets/Scripts/Debuffs/Slow_Debuff.cs
+++ Assets/Scripts/Debuffs/Slow_Debuff.cs
@@ -5,11 +5,7 @@ using UnityEngine;
 public class Slow_Debuff : Movement_Debuff {
 
     private void Awake() {
-        if (PlayerData.secrets.slow_resistance >= base.slow_percentage) {
-            base.slow_factor = 1;
-        }
-        else {
-            base.slow_factor = 1 - (base.slow_percentage - PlayerData.secrets.slow_resistance) / 100;
-        }
+        base.slow_factor = PlayerData.secrets.slow_resistance >= base.slow_percentage ? 1 : 
+                           1 - (base.slow_percentage - PlayerData.secrets.slow_resistance) / 100;
     }
 }
