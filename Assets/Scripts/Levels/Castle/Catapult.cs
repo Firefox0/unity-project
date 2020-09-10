44409bd38b00b73ff4d601505555d0fe81d4fa0a
Shao <shao-chieh.lee@campus.tu-berlin.de>
2020-09-10 15:31:35 +0200

    added head_projectile & poison debuff

--- Assets/Scripts/Levels/Castle/Catapult.cs
+++ Assets/Scripts/Levels/Castle/Catapult.cs
@@ -1,13 +1,14 @@
-﻿using UnityEngine;
+﻿using System.Dynamic;
+using UnityEngine;
 
 public class Catapult : MonoBehaviour {
 
+    public GameObject[] projectile_prefabs;
     public GameObject catapult;
     public GameObject player;
-    public GameObject stonePrefab;
 
-    private float fireRate = 2.0F;
-    private float nextStone = 2.0F;
+    private float fire_rate = 2.0F;
+    private float next_projectile = 2.0F;
 
     // Start is called before the first frame update
     void Start() {
@@ -26,12 +27,19 @@ public class Catapult : MonoBehaviour {
         this.transform.up = new Vector2(playerX - this.transform.position.x, playerY - this.transform.position.y);
     }
 
+    public int get_random_projectile() {
+        return Random.Range(0, this.projectile_prefabs.Length);
+    }
+
     public void shoot() {
-        if (Time.time > nextStone) {
-            nextStone = Time.time + fireRate;
-            GameObject stone = Instantiate(stonePrefab, catapult.transform.position, catapult.transform.rotation);
-            Rigidbody2D rigidbody = stone.GetComponent<Rigidbody2D>();
-            rigidbody.AddForce(transform.up * stonePrefab.GetComponent<Basic_Projectile>().speed, ForceMode2D.Impulse);
+        // get random projectile from projectile_prefabs array
+        GameObject current_projectile = projectile_prefabs[get_random_projectile()];
+        // shoot next projectile
+        if (Time.time > next_projectile) {
+            next_projectile = Time.time + fire_rate;
+            GameObject projectile = Instantiate(current_projectile, catapult.transform.position, catapult.transform.rotation);
+            Rigidbody2D rigidbody = projectile.GetComponent<Rigidbody2D>();
+            rigidbody.AddForce(transform.up * current_projectile.GetComponent<Basic_Projectile>().speed, ForceMode2D.Impulse);
             
         }
     }
---
+++ Assets/Scripts/Projectile/Cloud_Of_Poison.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class Cloud_Of_Poison : MonoBehaviour
+{
+    public float time;
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
