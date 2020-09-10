096ca984fbd9c59752344bdf339a19a3687c96ae
Firefox0 <iGIwos7z7M0OL5@protonmail.com>
2020-09-10 07:30:55 +0200

    added maximum and error message

--- Assets/Scripts/UI/ShopScene.cs
+++ Assets/Scripts/UI/ShopScene.cs
@@ -25,13 +25,15 @@ public class ShopScene : MonoBehaviour {
     public TextMeshProUGUI stun_text;
     public TextMeshProUGUI money_text;
 
+    public TextMeshProUGUI error;
+
     void Start() {
-        this.health_button.onClick.AddListener(() => this.buy(ref health_text, ref PlayerData.secrets.health, 5));
-        this.stamina_button.onClick.AddListener(() => this.buy(ref stamina_text, ref PlayerData.secrets.stamina, 5));
-        this.walking_button.onClick.AddListener(() => this.buy(ref walking_text, ref PlayerData.secrets.walking_speed, 0.1f));
-        this.running_button.onClick.AddListener(() => this.buy(ref running_text, ref PlayerData.secrets.running_speed, 0.1f));
-        this.slow_button.onClick.AddListener(() => this.buy(ref slow_text, ref PlayerData.secrets.slow_resistance, 0.5f));
-        this.stun_button.onClick.AddListener(() => this.buy(ref stun_text, ref PlayerData.secrets.stun_resistance, 0.5f));
+        this.health_button.onClick.AddListener(() => this.buy(ref health_text, ref PlayerData.secrets.health, 5, 1000));
+        this.stamina_button.onClick.AddListener(() => this.buy(ref stamina_text, ref PlayerData.secrets.stamina, 5, 1000));
+        this.walking_button.onClick.AddListener(() => this.buy(ref walking_text, ref PlayerData.secrets.walking_speed, 0.1f, 10));
+        this.running_button.onClick.AddListener(() => this.buy(ref running_text, ref PlayerData.secrets.running_speed, 0.1f, 12.5f));
+        this.slow_button.onClick.AddListener(() => this.buy(ref slow_text, ref PlayerData.secrets.slow_resistance, 0.5f, 100));
+        this.stun_button.onClick.AddListener(() => this.buy(ref stun_text, ref PlayerData.secrets.stun_resistance, 0.5f, 100));
         this.back_button.onClick.AddListener(() => SceneManager.LoadScene(PlayerData.scene_index));
 
         health_text.text = PlayerData.secrets.health.ToString();
@@ -43,9 +45,17 @@ public class ShopScene : MonoBehaviour {
         money_text.text = PlayerData.secrets.currency.ToString();
     }
 
-    void buy(ref TextMeshProUGUI text_gui, ref float stat, float upgrade) {
-        if (PlayerData.secrets.currency < 1) {
+    void buy(ref TextMeshProUGUI text_gui, ref float stat, float upgrade, float maximum) {
+        if (PlayerData.secrets.currency <= 0) {
+            error.text = "You don't have enough money to buy this.";
             return;
+        }
+        else if (stat == maximum) {
+            error.text = "You can't buy this. The Maximum was reached.";
+            return;
+        }
+        else {
+            error.text = "";
         }
 
         PlayerData.secrets.currency -= 1;
